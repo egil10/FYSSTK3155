01fe0154a5e6077f5110c17584a50d3ee0e14dbc
aadnero <aadnero@ulrik.uio.no>
2025-10-01 09:43:30 +0200

    Implemented grad_desc_OLS with momentum

--- PROJECT 1/Code/gradient_descent.py
+++ PROJECT 1/Code/gradient_descent.py
@@ -76,9 +76,51 @@ def gradient_descent_Ridge(X, y, eta=0.01, lam=1, num_iters=1000, print_num_iter
         else: theta = theta_new
     return theta
 
-def momentum_gradient_descent_OLS():
-    pass
 
+
+def momentum_gradient_descent_OLS(X, y, eta=0.01, momentum = 0.3 ,num_iters=1000, print_num_iters = False):
+    """
+    Perform gradient descent with momentum to estimate coefficients for Ordinary Least Squares (OLS) regression.
+
+    Parameters
+    ----------
+    X : numpy.ndarray, shape (n_samples, n_features)
+        The input feature matrix.
+    y : numpy.ndarray, shape (n_samples,)
+        The target values.
+    eta : float, default=0.01
+        Learning rate that controls the step size in the gradient descent updates.
+    num_iters : int, default=1000
+        Number of iterations to run the gradient descent algorithm.
+    print_num_iters : bool, default=False
+        If True, prints the actual number of iterations taken before convergence.
+
+    Returns
+    -------
+    theta : numpy.ndarray, shape (n_features,)
+        Estimated regression coefficients after gradient descent optimization.
+    """
+
+    n_samples, n_features = X.shape
+    theta = np.zeros(n_features)
+    change = np.zeros(n_features)
+    for t in range(num_iters):
+        # Compute gradients for OLS
+        grad_OLS = 2/n_samples * X.T @ (X @ theta - y)
+        # Compute new change
+        new_change = eta*grad_OLS + momentum*change
+        # Update parameters theta
+        theta_new = theta - new_change
+        change = new_change
+        
+        # Stopping criterion:
+        if np.allclose(theta, theta_new, rtol=1e-12, atol=1e-12):
+            if print_num_iters:
+                print("Number of iterations: ", t+1)
+            return theta_new
+        else: theta = theta_new
+    return theta
+        
+        
 def momentum_gradient_descent_Ridge():
     pass
-        
