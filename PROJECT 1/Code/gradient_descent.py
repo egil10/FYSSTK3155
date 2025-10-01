ed1d37a750c6e34d3d0afbb78404f9416e5057fa
aadnero <aadnero@ulrik.uio.no>
2025-10-01 09:11:40 +0200

    Update grad_desc_OLS
    Added stopping criterion

--- PROJECT 1/Code/gradient_descent.py
+++ PROJECT 1/Code/gradient_descent.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def gradient_descent_OLS(X, y, eta=0.01, num_iters=1000):
+def gradient_descent_OLS(X, y, eta=0.01, num_iters=1000, print_num_iters = False):
     """
     Perform gradient descent to estimate coefficients for Ordinary Least Squares (OLS) regression.
 
@@ -14,29 +14,29 @@ def gradient_descent_OLS(X, y, eta=0.01, num_iters=1000):
         Learning rate that controls the step size in the gradient descent updates.
     num_iters : int, default=1000
         Number of iterations to run the gradient descent algorithm.
+    print_num_iters : bool, default=False
+        If True, prints the actual number of iterations taken before convergence.
 
     Returns
     -------
-    theta_gdOLS : numpy.ndarray, shape (n_features,)
+    theta : numpy.ndarray, shape (n_features,)
         Estimated regression coefficients after gradient descent optimization.
-
-    Notes
-    -----
-    - This implementation uses the squared error loss function.
-    - The gradient update rule is:
-          theta <- theta - eta * (2/n) * X.T @ (X @ theta - y)
-    - It does not include an intercept term by default. If an intercept is desired,
-      a column of ones should be added to `X` before calling this function.
     """
 
     n_samples, n_features = X.shape
-    theta_gdOLS = np.zeros(n_features)
+    theta = np.zeros(n_features)
     for t in range(num_iters):
         # Compute gradients for OSL and Ridge
-        grad_OLS = 2/n_samples * X.T @ (X @ theta_gdOLS - y)
+        grad_OLS = 2/n_samples * X.T @ (X @ theta - y)
         # Update parameters theta
-        theta_gdOLS = theta_gdOLS - eta*grad_OLS
-    return theta_gdOLS
+        theta_new = theta - eta*grad_OLS
+        # Stopping criterion:
+        if np.allclose(theta, theta_new, rtol=1e-12, atol=1e-12):
+            if print_num_iters:
+                print("Number of iterations: ", t+1)
+            return theta_new
+        else: theta = theta_new
+    return theta
 
 def gradient_descent_Ridge(X, y, eta=0.01, lam=1, num_iters=1000, print_num_iters = False):
     """
@@ -75,4 +75,10 @@ def gradient_descent_Ridge(X, y, eta=0.01, lam=1, num_iters=1000, print_num_iter
             return theta_new
         else: theta = theta_new
     return theta
+
+def momentum_gradient_descent_OLS():
+    pass
+
+def momentum_gradient_descent_Ridge():
+    pass
         
